509c14abe86856d7c021e3887a01eb833c13fdb3
leejinkwon <ehdsp7@gmail.com>
2020-05-03 15:18:09 +0900

    okpo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    
+
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
@@ -13,7 +13,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          하이하이222
+          하이하이222444
         </h1>
 
         <p class="subheading font-weight-regular">
